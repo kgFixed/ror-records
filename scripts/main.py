eddbe2bb37c04944c3776147b48fd81fbaa49fe0
antoine <antoinprovain@gmail.com>
2025-10-15 15:25:01 +0200

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,9 +4,8 @@ from json_to_ttl import get_json_raw_urls
 if __name__ == "__main__":
 
   # variables
-  release_name = "latest" 
   output_dir = "output/"
   
-  urls = get_json_raw_urls(release_name) 
+  urls = get_json_raw_urls() 
   print(urls)
   # process_from_url(urls, output_dir)
