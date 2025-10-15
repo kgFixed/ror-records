bfeabe31a4cc36498d88e4b227800aaec6ce8df2
antoine <antoinprovain@gmail.com>
2025-10-15 09:17:29 +0200

    Update create_rdf_file.py

--- .github/workflows/scripts/create_rdf_file.py
+++ .github/workflows/scripts/create_rdf_file.py
@@ -8,6 +8,9 @@ import logging
 logging.getLogger("sema.subyt").setLevel(logging.ERROR) 
 
 def json_to_individual_rdf(json_path, template_path, output_dir):
+    print(json_path)
+    print(template_path)
+    print(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
     
     with open(json_path, encoding='utf-8') as f:
@@ -21,17 +24,17 @@ def json_to_individual_rdf(json_path, template_path, output_dir):
                 json.dump({"sets": {"qres": [org]}}, tmp, ensure_ascii=False, indent=2)
                 tmp_path = tmp.name
             
-            try:
-                Subyt(
-                    template_name=Path(template_path).name,
-                    template_folder=str(Path(template_path).parent),
-                    extra_sources={"qres": str(Path(tmp_path).resolve())},
-                    sink=str(output_dir / f"{ror_id}.ttl"),
-                    overwrite_sink=True,
-                    conditional=False
-                ).process()
-            finally:
-                Path(tmp_path).unlink()
+            # try:
+            #     Subyt(
+            #         template_name=Path(template_path).name,
+            #         template_folder=str(Path(template_path).parent),
+            #         extra_sources={"qres": str(Path(tmp_path).resolve())},
+            #         sink=str(output_dir / f"{ror_id}.ttl"),
+            #         overwrite_sink=True,
+            #         conditional=False
+            #     ).process()
+            # finally:
+            #     Path(tmp_path).unlink()
 
 # Example of use for 2.1
 # json_to_individual_rdf( 
@@ -46,3 +49,4 @@ def json_to_individual_rdf(json_path, template_path, output_dir):
 #     template_path= Path(__file__).parent.parent / "template/template_1_0.ttl",
 #     output_dir= Path(__file__).parent.parent / "to_push"
 # )
+
