33d27ebf6fa4d3689d102a1fa34e7f640842dc05
antoine <antoinprovain@gmail.com>
2025-10-15 09:23:40 +0200

    Update create_rdf_file.py

--- .github/workflows/scripts/create_rdf_file.py
+++ .github/workflows/scripts/create_rdf_file.py
@@ -24,17 +24,17 @@ def json_to_individual_rdf(json_path, template_path, output_dir):
                 json.dump({"sets": {"qres": [org]}}, tmp, ensure_ascii=False, indent=2)
                 tmp_path = tmp.name
             
-            # try:
-            #     Subyt(
-            #         template_name=Path(template_path).name,
-            #         template_folder=str(Path(template_path).parent),
-            #         extra_sources={"qres": str(Path(tmp_path).resolve())},
-            #         sink=str(output_dir / f"{ror_id}.ttl"),
-            #         overwrite_sink=True,
-            #         conditional=False
-            #     ).process()
-            # finally:
-            #     Path(tmp_path).unlink()
+            try:
+                Subyt(
+                    template_name=Path(template_path).name,
+                    template_folder=str(Path(template_path).parent),
+                    extra_sources={"qres": str(Path(tmp_path).resolve())},
+                    sink=str(output_dir / f"{ror_id}.ttl"),
+                    overwrite_sink=True,
+                    conditional=False
+                ).process()
+            finally:
+                Path(tmp_path).unlink()
 
 # Example of use for 2.1
 # json_to_individual_rdf( 
@@ -49,5 +49,6 @@ def json_to_individual_rdf(json_path, template_path, output_dir):
 #     template_path= Path(__file__).parent.parent / "template/template_1_0.ttl",
 #     output_dir= Path(__file__).parent.parent / "to_push"
 # )
+
 
 
